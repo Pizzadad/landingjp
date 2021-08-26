cc482f25cf4571b2f8fe232ba8129d6029bb0928
CHRISTIAN JOSUE ROMERO LEON <cromero@tisoluciones.net.pe>
2021-08-26 09:15:02 -0500

    add tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,19 +5,10 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-H5JS8V3G94", // Google Analytics / GA
-          
-        ],
-        
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-        },
+        id: "GTM-TS7MWP3",
+        includeInDevelopment: false,
       },
     },
   "gatsby-plugin-gatsby-cloud",
